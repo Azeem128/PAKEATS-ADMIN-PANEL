2572bb34d28add2b357fb7e66bd21877f9471ec5
Azeem128 <146712750+Azeem128@users.noreply.github.com>
2025-05-05 18:09:13 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
     return (
       <div className="p-4">
         <h1 className="text-2xl font-bold">Home Page</h1>
-        <p>Welcome to the home page!</p>
+        <p>Welcome to the home page-Dear Admin!</p>
       </div>
     );
   }
